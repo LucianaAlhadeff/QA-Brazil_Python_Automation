78ac97b631b6e88d118fc8393766d73503d822bd
Luciana Alhadeff <alhadeffluciana@gmail.com>
2026-09-06 18:51:23 -0300

    Subindo Projeto sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-81aaaca5-24e0-4b26-96e0-ef62de53f21f.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM =	'East 2nd Street, 601'
+ADDRESS_TO =	'1300 1st St'
+PHONE_NUMBER =	'+1 123 123 12 12'
+CARD_NUMBER =	'1234 5678 9100'
+CARD_CODE =	'1111'
+MESSAGE_FOR_DRIVER =	'Pare no bar de sucos'
